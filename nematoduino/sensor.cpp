ebc46fecaaaeae90151066aeb1d4168e67018c1a
Nathan Griffith <nategri@gmail.com>
2019-02-21 21:49:46 -0800

    use IR as part of sensor suite

--- nematoduino/sensor.cpp
+++ nematoduino/sensor.cpp
@@ -1,16 +1,18 @@
 #include "sensor.hpp"
 
 Sensor::Sensor(long threshold) {
-  this->_distanceThreshold = threshold;
+  this->_ultrasonicDistanceThreshold = threshold;
+
+  Wire.begin();
 
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
 }
 
 uint8_t Sensor::triggered() {
-  long d = this->_detectDistance();
+  long d = this->_ultrasonicDetectDistance();
 
-  if(d<this->_distanceThreshold) {
+  if((d < this->_ultrasonicDistanceThreshold) || (this->_infraredTriggered())) {
     return 1;
   }
   else {
@@ -18,7 +20,38 @@ uint8_t Sensor::triggered() {
   }
 }
 
-long Sensor::_detectDistance() {
+uint8_t Sensor::_infraredI2CRead() {
+  Wire.requestFrom(I2C_ADDR, 1);
+
+  if(Wire.available()) {
+    return Wire.read();
+  }
+  else {
+    return 0xFF;
+  }
+}
+
+void Sensor::_infraredI2CWrite(uint8_t data) {
+  Wire.beginTransmission(I2C_ADDR);
+  Wire.write(data);
+  Wire.endTransmission();
+}
+
+uint8_t Sensor::_infraredTriggered() {
+  uint8_t data = 0xC0 | this->_infraredI2CRead();
+  this->_infraredI2CWrite(data);
+
+  uint8_t sensorValue = 0x3F | this->_infraredI2CRead();
+
+  if(sensorValue != 0xFF) {
+    return 1;
+  }
+  else {
+    return 0;
+  }
+}
+
+long Sensor::_ultrasonicDetectDistance() {
   // Send sound pulse
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
--- nematoduino/sensor.hpp
+++ nematoduino/sensor.hpp
@@ -1,5 +1,11 @@
-// Reference: http://www.instructables.com/id/Simple-Arduino-and-HC-SR04-Example/
+// References:
+// http://www.instructables.com/id/Simple-Arduino-and-HC-SR04-Example/
+// https://www.waveshare.com/wiki/AlphaBot2-Ar (demo code)
+
 #include <Arduino.h>
+#include <Wire.h>
+
+#define I2C_ADDR 0x20
 
 #define ECHO_PIN 2 // RX
 #define TRIG_PIN 3 // TX
@@ -10,6 +16,10 @@ class Sensor {
     uint8_t triggered();
 
   private:
-    long _detectDistance();
-    long _distanceThreshold;
+    uint8_t _infraredI2CRead();
+    void _infraredI2CWrite(uint8_t);
+    uint8_t _infraredTriggered();
+
+    long _ultrasonicDetectDistance();
+    long _ultrasonicDistanceThreshold;
 };
